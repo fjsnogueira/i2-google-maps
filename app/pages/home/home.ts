3f093b3aca0b8509b7922c1ee9be166f66a4a621
Chris Klanac <cklanac@gmail.com>
2016-07-27 13:15:10 -0400

    Basic Google Maps

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -5,7 +5,23 @@ import {NavController} from 'ionic-angular';
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
+  map: any;
   constructor(private navCtrl: NavController) {
+    this.map = null;
+  }
 
+  ngOnInit() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    var latLng = new google.maps.LatLng(41.8781, -87.6298);//Coordinates for Chicago
+    var mapOptions = {
+      center: latLng,
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    new google.maps.Map(document.getElementById("map"), mapOptions);
   }
 }
