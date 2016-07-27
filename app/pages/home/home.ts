b89a0f95ac8a349ed352cececc9292e50c1a089e
Chris Klanac <cklanac@gmail.com>
2016-07-27 13:45:51 -0400

    Using Geolocation plugin

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -14,6 +14,7 @@ export class HomePage {
     this.loadMap();
   }
 
+  /* Basic map with hard-coded coordinates
   loadMap() {
     var latLng = new google.maps.LatLng(41.8781, -87.6298);//Coordinates for Chicago
     var mapOptions = {
@@ -23,5 +24,54 @@ export class HomePage {
     }
 
     new google.maps.Map(document.getElementById("map"), mapOptions);
+  }
+  */
+
+  // Geolocation plugin
+  loadMap() {
+
+    let options = { timeout: 10000, enableHighAccuracy: true };
+
+    navigator.geolocation.getCurrentPosition(
+
+      (position) => {
+        var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+        var mapOptions = {
+          center: latLng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        this.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      },
+      (error) => {
+        console.log(error);
+      },
+      options);
+  }
+  addMarker() {
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+    let content = marker.getPosition().toString();
+
+    this.addInfoWindow(marker, content);
+
+  }
+  addInfoWindow(marker, content) {
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+      infoWindow.open(this.map, marker);
+    });
+
   }
 }
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Simple GoogleMaps in Ionic 2 app
+
+### Create App
+`ionic start i2-google-maps blank --v2 --ts`
+
+### Install Geolocation Plugin
+`ionic plugin add cordova-plugin-geolocation`
+
+###Provide Google Maps typings
+```
+npm install -g typings 
+typings install dt~google.maps --global
+```
+
+### References 
+* https://developers.google.com/maps/
+* http://www.joshmorony.com/ionic-2-how-to-use-google-maps-geolocation-video-tutorial/
